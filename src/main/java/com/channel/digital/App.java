3368d61d8756543feb2069519f822c6dc8f1cc91
jordanvieno <jordanvienosimamora@gmail.com>
2026-09-10 08:33:56 +0700

    nambahin warna biar setiap cek port sesuai tahap deployment versi developmentlinux

--- src/main/java/com/channel/digital/App.java
+++ src/main/java/com/channel/digital/App.java
@@ -7,28 +7,29 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-public class App {
-    public static void main(String[] args) throws IOException {
+public class App
+{
+    public static void main( String[] args ) throws IOException
+    {
         int port = 8080;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 
         String env = System.getenv("APP_ENV");
-        if (env == null)
-            env = "unknown";
+        if (env == null) env = "unknown";
 
         String color;
         switch (env) {
             case "production":
-                color = "#2ecc71"; // hijau
+                color = "#2ecc71";
                 break;
             case "testing":
-                color = "#f1c40f"; // kuning
+                color = "#f1c40f";
                 break;
             case "development":
-                color = "#e74c3c"; // merah
+                color = "#e74c3c";
                 break;
             default:
-                color = "#95a5a6"; // abu-abu, kalau env tidak dikenali
+                color = "#95a5a6";
         }
 
         final String finalEnv = env;
@@ -36,13 +37,12 @@ public class App {
 
         server.createContext("/", exchange -> {
             String html = "<html>"
-                    + "<head><title>Agen46 Backend</title></head>"
-                    + "<body style='background-color:" + finalColor
-                    + "; color:white; font-family:sans-serif; text-align:center; padding-top:100px;'>"
-                    + "<h1>Agen46 Backend</h1>"
-                    + "<h2>Environment: " + finalEnv.toUpperCase() + "</h2>"
-                    + "<p>Build: " + System.getenv().getOrDefault("BUILD_NUMBER", "N/A") + "</p>"
-                    + "</body></html>";
+                + "<head><title>Agen46 Backend</title></head>"
+                + "<body style='background-color:" + finalColor + "; color:white; font-family:sans-serif; text-align:center; padding-top:100px;'>"
+                + "<h1>Agen46 Backend</h1>"
+                + "<h2>Environment: " + finalEnv.toUpperCase() + "</h2>"
+                + "<p>Build: " + System.getenv().getOrDefault("BUILD_NUMBER", "N/A") + "</p>"
+                + "</body></html>";
             exchange.getResponseHeaders().set("Content-Type", "text/html");
             exchange.sendResponseHeaders(200, html.getBytes().length);
             OutputStream os = exchange.getResponseBody();
