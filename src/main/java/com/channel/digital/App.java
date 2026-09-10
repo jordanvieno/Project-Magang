aa17f876a75dbbde37370887aa3cb83ad7e4fa1b
jordanvieno <jordanvienosimamora@gmail.com>
2026-09-10 09:21:18 +0700

    perbaiki tujuan brach development

--- src/main/java/com/channel/digital/App.java
+++ src/main/java/com/channel/digital/App.java
@@ -24,7 +24,7 @@ public class App {
             case "testing":
                 color = "#f1c40f";
                 break;
-            case "developmentlinux":
+            case "developmen":
                 color = "#e74c3c";
                 break;
             default:
